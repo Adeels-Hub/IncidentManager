f3c6eac10ba283e0dbdf20bccd5bd53874b5b5a4
ASULEMAN <adeel.suleman@gmail.com>
2025-04-04 20:38:39 +1100

    Changes for github pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig({  
+  base: 'https://adeels-hub.github.io/incident-dashboard/',
   plugins: [react()],
-  server: {
-    port: 5173,
-  }
 });
