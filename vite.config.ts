365381b5ec741eba2a3ef4ee5e257757449ab233
ASULEMAN <adeel.suleman@gmail.com>
2025-04-07 15:16:39 +1000

    Prepare deployment: update .gitignore, package.json, vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({  
-  base: 'https://adeels-hub.github.io/incident-dashboard/',
+  base: '/incident-dashboard/',
   plugins: [react()],
 });
